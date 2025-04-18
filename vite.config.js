6789638707d363ac58f22eeff7894c44707e57c9
RamiAbdelhamid <ramighassan10@gmail.com>
2025-04-18 16:11:02 +0300

    update

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import { viteStaticCopy } from "vite-plugin-static-copy";
 
-
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react() , tailwindcss()],
-})
+  base: "/Portfolio/",
+  plugins: [
+    react(),
+    tailwindcss(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: "dist/index.html",
+          dest: "", // copy to dist/ as 404.html
+          rename: "404.html",
+        },
+      ],
+    }),
+  ],
+});
